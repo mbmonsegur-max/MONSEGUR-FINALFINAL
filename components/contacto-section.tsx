fe65af03e94fc6344da59e91acd7bed0b11e181d
mbmonsegur-max <mbmonsegur@gmail.com>
2026-05-26 09:19:27 -0400

    Update contacto-section.tsx

--- components/contacto-section.tsx
+++ components/contacto-section.tsx
@@ -45,7 +45,7 @@ export function ContactoSection() {
                 <Linkedin className="h-6 w-6" />
               </a>
               <a 
-                href="https://www.instagram.com/mariamonseguro/"
+                href="https://www.instagram.com/monseguro/" 
                 target="_blank" 
                 rel="noopener noreferrer"
                 className="w-12 h-12 bg-white rounded-lg flex items-center justify-center hover:scale-110 transition-transform overflow-hidden"
